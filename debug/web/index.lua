1e0c02a9ed7e7aeb854c555b348c6e93c1603b89
butland <55413843@qq.com>
2014-12-28 22:09:29 +0800

    增加模板功能

---
+++ debug/web/index.lua
@@ -0,0 +1,13 @@
+--ngx.header.content_type = "text/plain"
+local redis=require("redis")
+ngx.say(bit.tohex(9))
+local regex = "\\\\d+"
+local m = ngx.re.match("hello, 1234", regex)
+if m then ngx.say(m[0]) else ngx.say("not matched!") end
+ ngx.say("hello")
+local test="4534".."ÖÐ¹úÈË"
+ngx.say(test)
+
+local template = require "template"
+template.render("index.pl", { message = "Hello, World!" })
+
